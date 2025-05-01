2e53e682b087a0a35de74e3e8c898cfab632d0aa
thanakorn <thanakorn.7bc@gmail.com>
2025-05-02 02:35:35 +0700

    Switch to bcryptjs for Render compatibility

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
 const db = require('./db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const { error } = require('console');
 
